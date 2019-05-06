44ec1bd9adedf1052fe2a5545bb870c8b79f0f51
Kevin Andrade <kandrad2@binghamton.edu>
2019-05-06 17:32:31 -0400

    added comments

--- driver.cpp
+++ driver.cpp
@@ -12,10 +12,9 @@ int Capacity;
 std::vector<int> optimalItemsInBag;
 int totItems;
 //////////////////////////////////////
-
 void Input(string);
-int knapSack(int Capacity, int totItems, int ** K);
-void computeOptimalSolution(int ** K);
+int knapSackDynamic(int Capacity, int totItems);
+void itemsInBag();
 int KSMemoazation(int n, int c);
 
 // A utility function that will return the max of two integers
@@ -44,30 +43,29 @@ int main (int argc, char *argv[])
     K[row] = new int[Capacity+1];
   }
 cout << "******************** DYNAMIC PROGRAMMING APPROACH ********************" << endl;
-cout << "Capacity: " << Capacity << endl;;
+  cout << "Capacity: " << Capacity << endl;;
   auto start = high_resolution_clock::now();
-  cout << "Maximum profit is: " << knapSack(Capacity, totItems, K) << endl;
+  cout << "Maximum profit is: " << knapSackDynamic(Capacity, totItems) << endl;
   auto stop = high_resolution_clock::now();
   auto runtime = duration_cast<microseconds>(stop - start);
+
   cout << "Time: " << runtime.count() << "ms" << endl;
-  computeOptimalSolution(K);
+
+  itemsInBag();
   cout << "The items in the bag are: " ;
-  for(auto n : optimalItemsInBag)
-  {
+  for(auto n : optimalItemsInBag){
     cout <<"Item " <<  n << " ";
   }
   cout<< endl;
-////////////MEMOAZATION//////////////////////////////////
 
+//////////////////////////////////MEMOAZATION//////////////////////////////////
+/*Initializing 2D array*/
 arrMem = new int*[totItems+1];
-for(int row = 0; row < totItems+1; row++)
-{
+for(int row = 0; row < totItems+1; row++){
     arrMem[row]= new int[Capacity+1];
 }
-for(int i = 0; i < totItems+1; i++)
-{
-  for(int j =0; j < Capacity+1; j++)
-  {
+for(int i = 0; i < totItems+1; i++){
+  for(int j =0; j < Capacity+1; j++){
     arrMem[i][j] = -1;
   }
 }
@@ -80,11 +78,12 @@ auto start2 = high_resolution_clock::now();
 cout << "Maximum profit is: " << KSMemoazation(totItems, Capacity) << endl;
 auto stop2 = high_resolution_clock::now();
 auto runtime2 = duration_cast<microseconds>(stop2 - start2);
+
 cout << "Time: " << runtime2.count() << "ms" << endl;
 
 
-  for(int row = 0; row < totItems+1; row++)
-  {
+//Deleting arrays to avoid memory leaks
+  for(int row = 0; row < totItems+1; row++){
     delete [] K[row];
     delete [] arrMem[row];
   }
@@ -92,18 +91,7 @@ cout << "Time: " << runtime2.count() << "ms" << endl;
   delete [] arrMem;
 return 0;
 }
-/*
-void Input(string file, string w_p)
-{
-  ifstream File;
-  File.open(file);
-  int n;
-  while(File >> n){
-    if(w_p == "w") weight.push_back(n);
-    else profit.push_back(n);
-  }
-  File.close();
-}*/
+
 void Input(string file)
 {
   ifstream File;
@@ -117,23 +105,30 @@ void Input(string file)
   File.close();
 }
 
-/*
+/**
   O(nC)
+  where n is number of items and c is the capacity
+  c is not a function of the number of items
+  this is only good when C is relatively small in terms of n (i.e polynomial function of n rather than an exponential one)
 */
-int knapSack(int Capacity, int totItems, int ** K){
+int knapSackDynamic(int Capacity, int totItems){
   /*Building 2D Table in bottom up manner
-    row = to the item
-    col = to the current Capacity
+    row refers to the item
+    col refers to the current Capacity
   */
   for(int row = 0; row <= totItems; row++)
   {
     for(int col = 0; col <= Capacity; col++)
     {
-      if(row == 0 || col == 0) K[row][col] = 0; //this will initialize the first row and col to zeros
-      else if (weight[row-1] <= col) K[row][col] = max(profit[row-1] + K[row-1][col-weight[row-1]], K[row-1][col]); //this checks if the weight is at least equal to the capacity
-      else K[row][col] = K[row-1][col]; //if the weight is higher than the current capacity just add the previous row to this index
+      //this will initialize the first row and col to zeros
+      if(row == 0 || col == 0) K[row][col] = 0;
+      //this checks if the weight at i is at least equal to or less than the capacity of i
+      else if (weight[row-1] <= col) K[row][col] = max(profit[row-1] + K[row-1][col-weight[row-1]], K[row-1][col]);
+      //if the weight at i is higher than the current capacity just add the previous row to this index
+      else K[row][col] = K[row-1][col];
     }
   }
+  //max profit will be stored here
   return K[totItems][Capacity];
 }
 
@@ -141,7 +136,7 @@ int knapSack(int Capacity, int totItems, int ** K){
 /*
   Takes O(N) to compute
 */
-void computeOptimalSolution(int ** K)
+void itemsInBag()
 {
   int i = totItems;
   int c = Capacity;
@@ -162,6 +157,7 @@ void computeOptimalSolution(int ** K)
   n is equal to the number of items left,
   c is equal to the capacity left
   run time is also O(nC)
+  
 */
 int KSMemoazation(int n, int c)
 {
